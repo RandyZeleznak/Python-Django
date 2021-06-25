2c3a81e084759745fd67bf5b1068aaa9fa181310
Randy <RandyZeleznak@gmail.com>
2021-06-25 18:39:49 -0500

    062621:1830

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index),  # generates "/challenges/" path
+    path("", views.index, name="index"),  # generates "/challenges/" path
     path("<int:month>", views.monthly_challenge_number),
     path("<str:month>", views.monthly_challenge, name="monthly-challenges")
 ]
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,7 +1,8 @@
 from typing import Reversible
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404, HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from django.template.loader import render_to_string
 
 # Create your views here.
 
@@ -10,19 +11,22 @@ def index(request):
     list_items = ""
     months = list(monthly_challenges)
 
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse("monthly-challenges", args=[month])
-        list_items += f"<li> <a href =\"{month_path}\"> {capitalized_month} </a></li>"
+   # for month in months:
+   #     capitalized_month = month.capitalize()
+   #     month_path = reverse("monthly-challenges", args=[month])
+   #     list_items += f"<li> <a href =\"{month_path}\"> {capitalized_month} </a></li>"
 
-        response_data = f"<ul>{list_items}</ul>"
+   #     response_data = f"<ul>{list_items}</ul>"
 
-    return HttpResponse(response_data)
+    # return HttpResponse(response_data)
+    return render(request, "challenges/index.html", {
+        "months": months
+    })
 
 
 monthly_challenges = {
-    "january": "This works!",
-    "february": "This is February!",
+    "january": "Happy New Year!",
+    "february": None,
     "march": "Get ready for Baseball!",
     "april": "Start of Baseball",
     "may": "Rainy Rainy Rainy!",
@@ -32,7 +36,7 @@ monthly_challenges = {
     "september": "Get ready for Football!",
     "october": "Halloween",
     "november": "Happy Thanksgiving!",
-    "december": "Merry Christmas!!"
+    "december": "Merry Christmas"
 }
 
 
@@ -49,7 +53,14 @@ def monthly_challenge_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        response_data = f"<h1>{challenge_text}</h1>"
-        return HttpResponse(response_data)
+        return render(request, "challenges/challenge.html", {
+            "text": challenge_text,
+            "month_name": month.capitalize()
+        })
+      #  response_data = f"<h1>{challenge_text}</h1>"
+      #  response_data = render_to_string("challenges/challenge.html")
+      #  return HttpResponse(response_data)
     except:
-        return HttpResponse("<h1>This input not supported</h1>")
+        raise Http404()
+     #   response_data = render_to_string("404.html")
+     #   return HttpResponse(response_data)

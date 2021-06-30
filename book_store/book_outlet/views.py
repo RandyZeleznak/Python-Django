0eb138588e78adbad59c80e740918c203d6bdf12
Randy <RandyZeleznak@gmail.com>
2021-06-30 18:51:57 -0500

    063021:1850

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
+from .models import Book
+
 # Create your views here.
+
+
+def index(request):
+    books = Book.objects.all()
+    return render(request, "book_outlet/index.html", {
+        "books": books
+    })
+
+
+def book_detail(request, id):
+    return rendered(request, "book_outlet/book_detail.html", {
+        "title",
+        "author",
+        "rating",
+        "is_bestseller"
+    })

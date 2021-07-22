ecb0969991036896b9d60f8eab600e3c64126cd6
Randy <RandyZeleznak@gmail.com>
2021-07-22 09:24:16 -0500

    Section#9 Completed - 072221:0915

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,55 +1,6 @@
-from datetime import date
-
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Post
-
-all_posts = [
-    {
-        "slug": "hike-through-the-city",
-        "image": "ChicagoFireworks.jpg",
-        "author": "Randy Zeleznak",
-        "date": date(2021, 6, 15),
-        "title": "City Boating",
-        "excerpt": "There's nothing like a Downtown Lakefront view! I wasn't prepared for the wonderously beautiful views from the lake!",
-        "content": """
-    (1)Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque dolorum magni
-		aut consequuntur, eveniet debitis ducimus quasi veniam inventore at
-		asperiores ad porro modi optio totam eum iusto aliquid. Doloremque!
-    """
-    },
-    {
-        "slug": "hike-through-the-city2",
-        "image": "trebleheartbear.png",
-        "author": "Randy Zeleznak",
-        "date": date(2021, 6, 25),
-        "title": "City Hiking",
-        "excerpt": "(2)There's nothing like a Downtown Lakefront view! I wasn't prepared for the wonderously beautiful views from the lake!",
-        "content": """
-    (2)Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque dolorum magni
-		aut consequuntur, eveniet debitis ducimus quasi veniam inventore at
-		asperiores ad porro modi optio totam eum iusto aliquid. Doloremque!
-    """
-    },
-    {
-        "slug": "hike-through-the-city3",
-        "image": "springdemoone.png",
-        "author": "Randy Zeleznak",
-        "date": date(2021, 6, 28),
-        "title": "City Walking",
-        "excerpt": "(3)There's nothing like a Downtown Lakefront view! I wasn't prepared for the wonderously beautiful views from the lake!",
-        "content": """
-    (3)Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque dolorum magni
-		aut consequuntur, eveniet debitis ducimus quasi veniam inventore at
-		asperiores ad porro modi optio totam eum iusto aliquid. Doloremque!
-    """
-    }
-
-]
-
-
-def get_date(post):
-    return post['date']
 
 # Create your views here.
 
@@ -62,13 +13,15 @@ def starting_page(request):
 
 
 def posts(request):
+    all_posts = Post.objects.all().order_by("-date")
     return render(request, "blog/all-posts.html", {
         "all_posts": all_posts
     })
 
 
 def post_detail(request, slug):
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    identified_post = get_object_or_404(Post, slug=slug)
     return render(request, "blog/post-detail.html", {
-        "post": identified_post
+        "post": identified_post,
+        "post_tags": identified_post.tags.all()
     })
